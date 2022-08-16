f712c9291eaa5e1c6a93e51826ed87a9bf1244a3
Rafael Garcia <rago89@gmail.com>
2022-08-16 15:06:53 +0200

    Change update game route

--- api/routes/sub-routes/game.ts
+++ api/routes/sub-routes/game.ts
@@ -7,6 +7,7 @@ const routes: Router = express.Router();
 routes.get('/', gameController.getAll);
 routes.post('/level', gameController.postGame);
 routes.get('/:id', gameController.getOne);
-routes.put('/:id', gameController.updateGame);
+routes.put('/update/:id', gameController.updateGame);
+routes.delete('/delete/:id', gameController.updateGame);
 
 export default routes;
