056524fb7b03de7096ab44fc52584d17b36ec3ef
Rafael Garcia <rago89@gmail.com>
2022-08-17 09:09:09 +0200

    add tokenChecker users routes

--- api/routes/sub-routes/users.ts
+++ api/routes/sub-routes/users.ts
@@ -2,13 +2,19 @@ import { Router } from 'express';
 import express from 'express';
 import upload from '../../middleware/multer-avatar';
 import usersController from './../../controllers/users';
+import tokenChecker from '../../middleware/token-login';
 
 const routes: Router = express.Router();
 
 routes.post('/register', usersController.createUser);
-routes.get('/', usersController.getAll);
-routes.get('/:id', usersController.getOne);
-routes.put('/update/:id', upload.single('avatar'), usersController.updateUser);
-routes.delete('/delete/:id', usersController.deleteOne);
+routes.get('/', tokenChecker, usersController.getAll);
+routes.get('/:id', tokenChecker, usersController.getOne);
+routes.put(
+  '/update/:id',
+  tokenChecker,
+  upload.single('avatar'),
+  usersController.updateUser
+);
+routes.delete('/delete/:id', tokenChecker, usersController.deleteOne);
 
 export default routes;
